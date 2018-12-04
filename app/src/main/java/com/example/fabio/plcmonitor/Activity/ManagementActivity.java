d8041a907df07eb3d2c1369aed889faa468cae5f
Fabio Cumbo <legacy1810@gmail.com>
2018-12-04 16:42:37 +0100

    Ajout des boutons dans le layout management et les évenements onclick

--- app/src/main/java/com/example/fabio/plcmonitor/Activity/ManagementActivity.java
+++ app/src/main/java/com/example/fabio/plcmonitor/Activity/ManagementActivity.java
@@ -2,6 +2,7 @@ package com.example.fabio.plcmonitor.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.fabio.plcmonitor.R;
 
@@ -11,5 +12,20 @@ public class ManagementActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_management);
+    }
+
+    public void btnParamClick(View v)
+    {
+
+    }
+
+    public void btnCompClick(View v)
+    {
+
+    }
+
+    public void btnRegulClick(View v)
+    {
+
     }
 }
