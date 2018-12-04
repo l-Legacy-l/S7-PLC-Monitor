24ccb2589ff239a75873c912df6f4a9a9a724abc
Fabio Cumbo <legacy1810@gmail.com>
2018-12-04 17:06:35 +0100

    Demande de confirmation dans le cas où l'utilisateur appuie sur retour dans l'activité de management

--- app/src/main/java/com/example/fabio/plcmonitor/Activity/ManagementActivity.java
+++ app/src/main/java/com/example/fabio/plcmonitor/Activity/ManagementActivity.java
@@ -1,7 +1,11 @@
 package com.example.fabio.plcmonitor.Activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 
 import com.example.fabio.plcmonitor.R;
@@ -27,5 +31,32 @@ public class ManagementActivity extends AppCompatActivity {
     public void btnRegulClick(View v)
     {
 
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(ManagementActivity.this);
+            builder.setMessage("Voulez vous vous déconnecter ?");
+            builder.setCancelable(true);
+            builder.setNegativeButton("Non", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
+                }
+            });
+            builder.setPositiveButton("Oui", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        }
+        return false;
     }
 }
