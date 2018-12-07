c57c13386477da6651e48c86607bd04a580e7830
Fabio Cumbo <legacy1810@gmail.com>
2018-12-07 16:28:26 +0100

    Ajout de l'activity de management des utilisateurs

---
+++ app/src/main/java/com/example/fabio/plcmonitor/Activity/ManagementUserActivity.java
@@ -0,0 +1,15 @@
+package com.example.fabio.plcmonitor.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.fabio.plcmonitor.R;
+
+public class ManagementUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_management_user);
+    }
+}
