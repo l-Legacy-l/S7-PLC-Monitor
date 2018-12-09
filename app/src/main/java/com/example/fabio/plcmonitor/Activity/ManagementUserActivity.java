cd7dd232e4807b3a282afa1331f4f1944edb8bde
Fabio Cumbo <legacy1810@gmail.com>
2018-12-09 17:36:04 +0100

    Le switch pour promouvoir un utilisateur en admin change en fonction de l'utilisateur selectionner depuis le spinner

--- app/src/main/java/com/example/fabio/plcmonitor/Activity/ManagementUserActivity.java
+++ app/src/main/java/com/example/fabio/plcmonitor/Activity/ManagementUserActivity.java
@@ -2,8 +2,11 @@ package com.example.fabio.plcmonitor.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Switch;
 
 import com.example.fabio.plcmonitor.BDD.User;
 import com.example.fabio.plcmonitor.BDD.UserAccessDB;
@@ -16,11 +19,45 @@ public class ManagementUserActivity extends AppCompatActivity {
     UserAccessDB db;
 
     Spinner spin;
+    Switch switchAdmin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_management_user);
         fillingSpin();
+
+        switchAdmin = (Switch) findViewById(R.id.sw_muser_admin);
+
+        db = new UserAccessDB(this);
+
+        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+
+                db.openForRead();
+                //On récupère l'user correspondant au mail qu'on a sélectionné
+                User user1 = db.getUser(spin.getSelectedItem().toString());
+                db.Close();
+                //On met en ON le switch si l'utilisateur est un administrateur
+                if (user1.getIsAdmin())
+                {
+                    switchAdmin.setChecked(true);
+                }
+
+                else
+                {
+                    switchAdmin.setChecked(false);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                System.out.println("item nothing");
+            }
+
+        });
     }
 
     private void fillingSpin()
@@ -44,4 +81,5 @@ public class ManagementUserActivity extends AppCompatActivity {
         adapters.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spin.setAdapter(adapters);
     }
+
 }
