772c6b3fdc4f1bf99846c44703526bb4e0d6f07c
Fabio Cumbo <legacy1810@gmail.com>
2018-12-23 23:23:57 +0100

    Ajout de méthodes pour se connecter et se déconnecter à l'automate. + méthode booléenne pour connaitre si un utilisateur est connecté ou non

--- app/src/main/java/com/example/fabio/plcmonitor/Automaton/ReadTaskS7.java
+++ app/src/main/java/com/example/fabio/plcmonitor/Automaton/ReadTaskS7.java
@@ -43,11 +43,14 @@ public class ReadTaskS7
 
     private byte[] datasPLC = new byte[512];
 
-    //Constructeur ReadTaskS7 pour les comprimés
+    //Variable permettant de déterminer si l'utilisateur est connecté à l'automate
+    public boolean isConnected= false;
 
+    //Constructeur ReadTaskS7 pour les comprimés
     public ReadTaskS7(View vi_comp_ui,TextView tv_comp_plcNumber, TextView tv_comp_nbBouteille, CheckBox cb_comp_service, CheckBox cb_comp_flacon,
                       Button bt_comp_5, Button bt_comp_10, Button bt_comp_15, ImageButton ib_comp_connexion)
     {
+        //Objets modifiés par la tâche de fond
         this.vi_comp_ui = vi_comp_ui;
         this.tv_comp_plcNumber = tv_comp_plcNumber;
         this.tv_comp_nbBouteille = tv_comp_nbBouteille;
@@ -61,7 +64,37 @@ public class ReadTaskS7
         comS7 = new S7Client();
         plcS7 = new AutomateS7();
 
+        //Thread de lecture d'informations
         readThread = new Thread(plcS7);
+    }
+
+    public boolean isConnected()
+    {
+        return isConnected;
+    }
+
+
+    public void Start(String ip, String rack, String slot) {
+        //Vérification si le thread n'est pas en cours
+        if(!readThread.isAlive()){
+            //Récupère les paramètres de connexion à l'API (IP, rack, slot)
+            param[0] = ip;
+            param[1] = rack;
+            param[2] = slot;
+
+            //Démare le thread et active le flag de gestion
+            readThread.start();
+            isRunning.set(true);
+        }
+    }
+
+    public void Stop(){
+        //Interruption du traitement (flag)
+        isRunning.set(false);
+        //Arrêt de communication avec l'API
+        comS7.Disconnect();
+        //interruption du Thread avant la fin de son traitement
+        readThread.interrupt();
     }
 
     private class AutomateS7 implements Runnable
