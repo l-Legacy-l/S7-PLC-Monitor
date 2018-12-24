7ce8a8397e148406782d4f2216e5c3f23b9f0b2d
Fabio Cumbo <legacy1810@gmail.com>
2018-12-24 01:04:31 +0100

    Connexion/Deconnexion via Wi-Fi Ok pour les comprimés, le bouton change de couleur

--- app/src/main/java/com/example/fabio/plcmonitor/Activity/AutomatonCompActivity.java
+++ app/src/main/java/com/example/fabio/plcmonitor/Activity/AutomatonCompActivity.java
@@ -1,6 +1,7 @@
 package com.example.fabio.plcmonitor.Activity;
 
 import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
@@ -10,8 +11,10 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.fabio.plcmonitor.Automaton.ReadTaskS7;
+import com.example.fabio.plcmonitor.Configs;
 import com.example.fabio.plcmonitor.R;
 
 public class AutomatonCompActivity extends AppCompatActivity
@@ -47,5 +50,61 @@ public class AutomatonCompActivity extends AppCompatActivity
         bt_comp_10 = (Button)findViewById(R.id.bt_comp_10);
         bt_comp_15 = (Button)findViewById(R.id.bt_comp_15);
         ib_comp_connexion = (ImageButton)findViewById(R.id.ib_comp_connexion);
+    }
+
+    public void onMainClickManager(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.ib_comp_connexion:
+                ColorDrawable ibBackgroundColor = (ColorDrawable) ib_comp_connexion.getBackground();
+                int colorId = ibBackgroundColor.getColor();
+
+                //On vérifie si on est déjà connecté ou non
+                if(colorId == getResources().getColor(R.color.red) || colorId == getResources().getColor(R.color.orange))
+                {
+                    if (network != null && network.isConnectedOrConnecting()) {
+                        Toast.makeText(getApplicationContext(), "Connecté en : " + network.getTypeName(), Toast.LENGTH_SHORT).show();
+
+                        try{
+                            readS7 = new ReadTaskS7(v, tv_comp_PLCnumber, tv_comp_nbBouteille, cb_comp_service, cb_comp_flacon, bt_comp_5,
+                                    bt_comp_10, bt_comp_15, ib_comp_connexion);
+                            readS7.Start(Configs.getIp(), Integer.toString(Configs.getRack()), Integer.toString(Configs.getSlot()));
+
+                            ib_comp_connexion.setBackgroundColor(getResources().getColor(R.color.green));
+
+                        }
+                        catch(Exception e){
+                            Toast.makeText(getApplicationContext(),"Une erreur s'est produite, veuillez recommencer", Toast.LENGTH_LONG).show();
+                            ib_comp_connexion.setBackgroundColor(getResources().getColor(R.color.orange));
+                        }
+                        /*
+                           if(!readS7.isConnected()){
+                            Toast.makeText(this,"Connexion impossible\nVérifiez l'automate", Toast.LENGTH_LONG).show();
+                            }else{
+                            }
+                           */
+
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        //writeS7 = new WriteTaskS7();
+                        //writeS7.Start(Globals.getIp(), Globals.getRack(), Globals.getSlot());
+
+                    }
+                    else
+                    {
+                        Toast.makeText(getApplicationContext(), "! Connexion réseau IMPOSSIBLE !", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                else
+                {
+                    //On est déjà connecté donc on veut se déconnecter
+                    ib_comp_connexion.setBackgroundColor(getResources().getColor(R.color.red));
+                }
+        }
     }
 }
