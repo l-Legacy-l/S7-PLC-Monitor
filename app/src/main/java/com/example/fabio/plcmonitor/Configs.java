ee4fc3d3aabfd7cfa4a557dc7d40abb692e45809
Fabio Cumbo <legacy1810@gmail.com>
2018-12-28 03:07:39 +0100

    Modification de la configuration IP de base et du datablock par défaut

--- app/src/main/java/com/example/fabio/plcmonitor/Configs.java
+++ app/src/main/java/com/example/fabio/plcmonitor/Configs.java
@@ -2,10 +2,10 @@ package com.example.fabio.plcmonitor;
 
 public class Configs
 {
-    static String ip = "192.168.0.1";
+    static String ip = "192.168.0.10";
     static int rack = 0;
     static int slot = 2;
-    static int datablock = 22;
+    static int datablock = 5;
 
     public static String getIp() {
         return ip;
